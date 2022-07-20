2d986a82b6ab76889a1ac3f8bf1e219a92e90d67
sotocode <sotocreate@gmail.com>
2022-07-20 06:25:35 -0400

    fixed JS issue and HTML

--- scripts.js
+++ scripts.js
@@ -1,1 +1,23 @@
-// Your code here
+const cat_result = document.getElementById('cat_result');
+const dog_result = document.getElementById('dog_result');
+
+function getRandomCat() {
+	fetch('https://aws.random.cat/meow')
+		.then(res => res.json())
+		.then(data => {
+			cat_result.innerHTML = `<img src=${data.file} alt="cat" class="img-fluid" />`
+		})
+}
+
+function getRandomDog() {
+	fetch('https://random.dog/woof.json')
+		.then(res => res.json())
+		.then(data => {
+			if(data.url.includes('.mp4')) {
+				getRandomDog();
+			}
+			else {
+				dog_result.innerHTML = `<img src=${data.url} alt="dog" class="img-fluid" />`
+			}
+		});
+}
