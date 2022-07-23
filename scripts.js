d3cef455f3bea7acbc9a57e7808e81d6348db0a0
Kate Liptak <kate.liptak@gmail.com>
2022-07-22 22:46:25 -0400

    localStorage

--- scripts.js
+++ scripts.js
@@ -1,10 +1,13 @@
 const cat_result = document.getElementById('cat_result');
 const dog_result = document.getElementById('dog_result');
+let prev_dogs = []
+let prev_cats = []
 
 function getRandomCat() {
 	fetch('https://aws.random.cat/meow')
 		.then(res => res.json())
 		.then(data => {
+			saveResult("cat", data.file)
 			cat_result.innerHTML = `<img src=${data.file} alt="cat" class="img-fluid" />`
 		})
 }
@@ -17,8 +20,36 @@ function getRandomDog() {
 				getRandomDog();
 			}
 			else {
+				saveResult("dog", data.url)
 				dog_result.innerHTML = `<img src=${data.url} alt="dog" class="img-fluid" />`
 			}
 		});
 }
 
+function saveResult(type, src){
+	if (type === "cat"){
+		if (prev_cats.length >= 5){
+			prev_cats.shift()
+		}
+		prev_cats.push(src)
+	} else {
+		if (prev_dogs.length >= 5){
+			prev_dogs.shift()
+		}
+		prev_dogs.push(src)
+	}
+	console.log(prev_dogs)
+	localStorage.setItem("prev_dogs", JSON.stringify(prev_dogs))
+	localStorage.setItem("prev_cats", JSON.stringify(prev_cats))
+}
+
+document.body.onload = async function() {
+	let stringCats = localStorage.getItem("prev_cats")
+	let stringDogs = localStorage.getItem("prev_dogs")
+	if (stringCats){
+		prev_cats = await JSON.parse(stringCats)
+	}
+	if (stringDogs){
+		prev_dogs = await JSON.parse(stringDogs)
+	}
+}
